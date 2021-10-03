10c219033c2c1902dca24f6d1519d403c2f158a6
SOUMITRA1208 <69211643+SOUMITRA1208@users.noreply.github.com>
2021-10-03 21:10:48 +0530

    Variables introduction

---
+++ cpp/variables intro.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum=6;  
+  
+    float sub=5.3;
+    char naam ='A';
+    cout<< sum ;
+    cout<<sub ;
+    cout<<naam;
+    return 0;
+}
+
